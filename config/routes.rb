30d68ac5483aa1405e3c5f373284bb7ba6bbf360
ryota miwa <wellinfrog@gmail.com>
2023-06-14 21:21:59 +0000

    kadai2-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  #PostController
+  get'posts/new',to:'post#new',as:'new_post'
+  get 'topics/new', to: 'topics#new',as:'new_topics'
+  get 'topics/edit', to: 'topics#edit',as:'edit_topics'
+  
 end
+
